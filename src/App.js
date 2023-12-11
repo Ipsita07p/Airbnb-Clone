b275dcdc70d25e8148aa1453f5bcefa5f23fd0ae
Ipsita07p <ipsitapati.0708@gmail.com>
2023-12-11 20:39:08 +0530

    learning

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
-import Navbar from "./components/Navnknmkbar";
-import Hero from "../src/components/Hero";
-import Card from "../src/components/Card"
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card"
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      <Card
+        img="katie-zaferes.png"
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   );
 }
