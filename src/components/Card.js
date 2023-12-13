55fca08cb400baf99128f47bdea46aa1319a8de8
Ipsita07p <ipsitapati.0708@gmail.com>
2023-12-13 13:05:51 +0530

    learning

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,32 @@
 import React from "react";
 
 export default function Card(props) {
-    return (
-        <div className="card">
-            <img src={`../images/${props.img}`} className="card--image" alt="product" />
-            <div className="card--stats">
-                <img src="../images/star.png" className="card--star" alt="star" />
-                <span>{props.rating}</span>
-                <span className="gray"> ({props.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>
-            </div>
-            <p className= "card--title">{props.title}</p>
-            <p className= "card--price"><span style={{fontWeight: "bold"}}>From ${props.price}</span> / person</p>
-        </div>
-    )
+  let badgeText;
+  if (props.item.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.item.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
+  return (
+    <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img
+        src={`../images/${props.item.coverImg}`}
+        className="card--image"
+        alt="product"
+      />
+      <div className="card--stats">
+        <img src="../images/star.png" className="card--star" alt="star" />
+        <span>{props.item.stats.rating}</span>
+        <span className="gray"> ({props.item.stats.reviewCount}) • </span>
+        <span className="gray">{props.item.location}</span>
+      </div>
+      <p className="card--title">{props.item.title}</p>
+      <p className="card--price">
+        <span style={{ fontWeight: "bold" }}>From ${props.item.price}</span> /
+        person
+      </p>
+    </div>
+  );
 }
